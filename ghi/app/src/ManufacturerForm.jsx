7f66a66c85ae4b652e45e88a8f769d173f301160
Daniel Kim <danjkim831@gmail.com>
2024-07-15 13:25:58 -0700

    Create Manufacturer form

--- ghi/app/src/ManufacturerForm.jsx
+++ ghi/app/src/ManufacturerForm.jsx
@@ -17,18 +17,58 @@ function ManufacturerForm() {
 
     // // Submit Handler (POST):
 
-    // const handleSubmit = async (event) => {
-    //     event.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
+        const data = {};
+        data.name = manufacturer;
 
-    // }
+        const url = "http://localhost:8100/api/manufacturers";
+        const fetchConfig = {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+        }
+
+        try {
+            const response = await fetch(url, fetchConfig);
+            if (response.ok) {
+                const newManufacturer = await response.json();
+                console.log(newManufacturer);
+
+                setManufacturer('');
+            } else {
+                const errorDetails = await response.json();
+                console.error(`Response status: ${response.status} ${response.statusText} // Error detail: ${errorDetails}`);
+            }
+        } catch (e) {
+            console.error(`Fetch error: ${e}`);
+        }
+    }
 
     // ==========================================
 
     // JSX:
 
     return (
-        <div>Loading Manufacturer Form</div>
+        <div className="row mt-5">
+            <div className="offset-3 col-6">
+                <div className="shadow p-4 mt-4">
+                    <h1>Create a new manufacturer</h1>
+                    <form onSubmit={handleSubmit} id="create-manufacturer-form">
+                        <div className="form-floating mb-3">
+                            <input onChange={handleNameChange} value={manufacturer} placeholder="Manufacturer" required type="text" name="manufacturer"
+                                id="manufacturer" className="form-control" />
+                            <label htmlFor="manufacturer">Manufacturer</label>
+                        </div>
+                        <button type="submit" className="btn btn-primary mt-2">Create</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+        // <div>hi</div>
     )
 }
 
