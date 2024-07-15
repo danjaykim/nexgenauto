ee92558de3318b583371ad03faebb1a744ccb6e7
Max Vuong <mace.max99@gmail.com>
2024-07-15 18:27:23 -0500

    got the API get and post for technicians working

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from .encoders import AutomobileVOEncoder, TechnicianEncoder, AppointmentEncoder
+from .models import AutomobileVO, Technician, Appointment
 
 # Create your views here.
+
+
+@require_http_methods(["GET", "POST"])
+def api_technician_list(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianEncoder,
+        )
+    else:
+        try:
+            content = json.loads(request.body)
+            tech = Technician.objects.create(**content)
+            return JsonResponse(
+                tech,
+                encoder=TechnicianEncoder,
+                safe=False,
+            )
+        except:
+            response = JsonResponse({"message": "Could not create the Technician"})
+            response.status_code = 400
+            return response
--- service/poll/poller.py
+++ service/poll/poller.py
@@ -15,14 +15,14 @@ from service_rest.models import AutomobileVO
 
 def get_automobiles():
     print("getting automobiles")
-    response = requests.get("http://inventory-api:8100/api/automobiles/")
+    # response = requests.get("http://localhost:8100/api/automobiles/")
     # print(response)
-    content = json.loads(response.content)
+    # content = json.loads(response.content)
     # print(content)
-    for item in content["autos"]:
-        AutomobileVO.objects.update_or_create(
-            defaults={"vin": item["vin"], "sold": item["sold"]}
-        )
+    # for item in content["autos"]:
+    #     AutomobileVO.objects.update_or_create(
+    #         defaults={"vin": item["vin"], "sold": item["sold"]}
+    #     )
 
 
 def poll():
@@ -33,7 +33,7 @@ def poll():
         except Exception as e:
             traceback.print_exc()
             print(e, file=sys.stderr)
-        time.sleep(5)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
