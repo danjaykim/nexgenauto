fc014e66bbd2a15cdb2781a6106f283136c77cf0
Max Vuong <mace.max99@gmail.com>
2024-07-15 17:10:17 -0500

    finished adding models to service API

---
+++ service/api/service_rest/encoders.py
@@ -0,0 +1,37 @@
+from common.json import ModelEncoder
+
+from .models import AutomobileVO, Technician, Appointment
+
+
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "id",
+        "vin",
+        "sold",
+        "import_href",
+    ]
+
+class TechnicianEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "id",
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+class AppointmentEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        "id",
+        "date_time",
+        "reason",
+        "status",
+        "vin",
+        "customer",
+        "technician",
+    ]
+    encoders = {
+        "technician": TechnicianEncoder(),
+    }
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+    def __str__(self):
+        return self.vin
+
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_api_url(self):
+        return reverse("api_technician", kwargs={"pk": self.id})
+
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, default="in progress")
+    vin = models.CharField(max_length=17)
+    customer = models.CharField(max_length=200)
+
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointment",
+        on_delete=models.CASCADE,
+        )
+
+    def __str__(self):
+        return f"{self.date_time} {self.customer}"
