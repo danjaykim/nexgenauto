4cb443666d0ce2ad05d7a991f5f0b09eef4f395e
Daniel Kim <danjkim831@gmail.com>
2024-07-16 21:53:53 -0700

    Minor refactor with ternary operator

--- ghi/app/src/AutoList.jsx
+++ ghi/app/src/AutoList.jsx
@@ -1,65 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function AutoList(){
-    const [autos, setAutos] = useState([])
-    const URL = "http://localhost:8100/api/automobiles/"
+export default function AutoList() {
+
+    // States:
+
+    const [autos, setAutos] = useState([]);
+
+    // =============================================
+
+    // Fetch automobile data:
+
     const fetchData = async () => {
-        const response = await fetch(URL)
-        if(response.ok){
-            const data = await response.json()
-            setAutos(data.autos)
+        const URL = "http://localhost:8100/api/automobiles/";
+        const response = await fetch(URL);
+        if (response.ok) {
+            const data = await response.json();
+            setAutos(data.autos);
         }
     }
-    useEffect(() => {fetchData()}, [])
-    if(autos === null){
-        return <div>Loading</div>
+    useEffect(() => { fetchData() }, [])
+
+    // =============================================
+
+    // JSX:
+
+    if (autos === null) {
+        return <div>Loading...</div>
     }
+
     return (
-        <>
-            <div>
-                <h1 className="display-5 fw-bold">Automobiles</h1>
-                <table className="table table-striped">
+        <div>
+            <h1 className="display-5 fw-bold">Automobiles</h1>
+            <table className="table table-striped">
                 <thead>
                     <tr>
-                    <th>VIN</th>
-                    <th>Color</th>
-                    <th>Year</th>
-                    <th>Model</th>
-                    <th>Manufacturer</th>
-                    <th>Sold</th>
+                        <th>VIN</th>
+                        <th>Color</th>
+                        <th>Year</th>
+                        <th>Model</th>
+                        <th>Manufacturer</th>
+                        <th>Sold</th>
                     </tr>
                 </thead>
                 <tbody>
                     {autos.map(auto => {
-                        if(auto.sold){
-                            return (
-                                <tr key={auto.id}>
-                                    <td>{ auto.vin }</td>
-                                    <td>{ auto.color }</td>
-                                    <td>{ auto.year }</td>
-                                    <td>{ auto.model.name }</td>
-                                    <td>{ auto.model.manufacturer.name }</td>
-                                    <td>Yes</td>
-                                </tr>
-                            );
-                        } else {
-                            return (
-                                <tr key={auto.id}>
-                                    <td>{ auto.vin }</td>
-                                    <td>{ auto.color }</td>
-                                    <td>{ auto.year }</td>
-                                    <td>{ auto.model.name }</td>
-                                    <td>{ auto.model.manufacturer.name }</td>
-                                    <td>No</td>
-                                </tr>
-                            )
-                        }
+                        return (
+                            <tr key={auto.id}>
+                                <td>{auto.vin}</td>
+                                <td>{auto.color}</td>
+                                <td>{auto.year}</td>
+                                <td>{auto.model.name}</td>
+                                <td>{auto.model.manufacturer.name}</td>
+                                <td>{auto.sold ? "SOLD" : "AVAILABLE"}</td>
+                            </tr>
+                        );
                     })}
                 </tbody>
-                </table>
-            </div>
-        </>
+            </table>
+        </div>
     )
 }
-
-export default AutoList
