0cd93b18cc3bddf1926ef05bcda98f91bf094e1b
Daniel Kim <danjkim831@gmail.com>
2024-07-23 17:04:20 -0700

    Added picture_url field to Manufacturer model, made migrations, migrated

--- inventory/api/inventory_rest/models.py
+++ inventory/api/inventory_rest/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    picture_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.name
