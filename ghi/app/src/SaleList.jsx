a83e684594782fc15c01e546d786e000d4aa0c4a
Daniel Kim <danjkim831@gmail.com>
2024-07-25 21:55:36 -0700

    Updated Sales list JSX to look cleaner. Will circle back

--- ghi/app/src/SaleList.jsx
+++ ghi/app/src/SaleList.jsx
@@ -33,31 +33,57 @@ export default function SalesList() {
 
     return (
         <div>
-            <h1 className="mt-4 mb-3">Sales</h1>
-            <table className="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Salesperson Employee ID</th>
-                        <th>Salesperson Name</th>
-                        <th>Customer</th>
-                        <th>VIN</th>
-                        <th>Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {sales.map(sale => {
-                        return (
-                            <tr key={sale.id}>
-                                <td>{sale.salesperson.employee_id}</td>
-                                <td>{sale.salesperson.first_name} {sale.salesperson.last_name}</td>
-                                <td>{sale.customer.first_name} {sale.customer.last_name}</td>
-                                <td>{sale.automobile.vin}</td>
-                                <td>${sale.price}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            <h2 className="text-center manu-title">Sales</h2>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                {sales.map(sale => {
+                    return (
+                        <div className="col" key={sale.id}>
+                            <div className="card sale-card">
+                                <div className="card-body text-center">
+                                    <h5>Sale ID: {sale.id}</h5>
+                                    <div>VIN: <b>{sale.automobile.vin}</b></div>
+                                    <div>Price Sold: <b>${sale.price}</b></div>
+                                    <hr />
+                                    <p>Salesperson Info:</p>
+                                    <div><b>{sale.salesperson.first_name} {sale.salesperson.last_name}</b></div>
+                                    <div>Employee ID: {sale.salesperson.id}</div>
+                                    <hr />
+                                    <p>Customer Info:</p>
+                                    <div><b>{sale.customer.first_name} {sale.customer.last_name}</b></div>
+                                    <div>Phone Number: {sale.customer.phone_number}</div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     )
 }
+// <div>
+//     <h1 className="mt-4 mb-3">Sales</h1>
+//     <table className="table table-striped table-hover">
+//         <thead>
+//             <tr>
+//                 <th>Salesperson Employee ID</th>
+//                 <th>Salesperson Name</th>
+//                 <th>Customer</th>
+//                 <th>VIN</th>
+//                 <th>Price</th>
+//             </tr>
+//         </thead>
+//         <tbody>
+//             {sales.map(sale => {
+//                 return (
+//                     <tr key={sale.id}>
+//                         <td>{sale.salesperson.employee_id}</td>
+//                         <td>{sale.salesperson.first_name} {sale.salesperson.last_name}</td>
+//                         <td>{sale.customer.first_name} {sale.customer.last_name}</td>
+//                         <td>{sale.automobile.vin}</td>
+//                         <td>${sale.price}</td>
+//                     </tr>
+//                 );
+//             })}
+//         </tbody>
+//     </table>
+// </div>
